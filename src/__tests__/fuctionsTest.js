4714866c62c803e5d41eb4fe7797ace625db14a0
Carlos Diaz <hola@cjdiaz.com>
2019-05-21 21:54:02 -0500

    Adding a third fuction to sum an Array an creating a Test fot Jest

--- src/__tests__/fuctionsTest.js
+++ src/__tests__/fuctionsTest.js
@@ -1,7 +1,30 @@
-const { getLargerInt } = require("../fuctions.js");
+const {
+  getLargerInt,
+  isEvenOrOdd,
+  calculateAverage
+} = require("../fuctions.js");
 
-describe("Functions Test", () => {
+describe("getLargerInt Test", () => {
   test("getLargerInt returns larger integer", () => {
-    expect(getLargerInt(2, 8)).toBe(8);
+    expect(getLargerInt(1, 2)).toBe(2);
+  });
+
+  test("getLargetInt returns udefined if no arguments provided", () => {
+    expect(getLargerInt()).toBeUndefined();
+  });
+});
+
+describe("isEvenOrOdd", () => {
+  test("isEvenOrOdd returns Even for 8", () => {
+    expect(isEvenOrOdd(10)).toBe("Even");
+  });
+  test("isEvenOrOdd return 'Odd' for 7", () => {
+    expect(isEvenOrOdd(7)).toBe("Odd");
+  });
+});
+
+describe("calculateAverage", () => {
+  test("Calculate Avarage from sum of an Array", () => {
+    expect(calculateAverage([1, 2, 3, 4, 5])).toBe(3);
   });
 });
--- src/fuctions.js
+++ src/fuctions.js
@@ -2,6 +2,22 @@ function getLargerInt(num1, num2) {
   return num1 > num2 ? num1 : num2;
 }
 
+// Saber si es PAR o IMP
+function isEvenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+
+// Promedio de numero deacuerdo al Array
+function calculateAverage(array) {
+  var sumaArray = 0;
+  for (i = 0; i < array.length; i++) {
+    sumaArray += array[i];
+  }
+  return sumaArray / array.length;
+}
+
 module.exports = {
-  getLargerInt
+  getLargerInt,
+  isEvenOrOdd,
+  calculateAverage
 };
